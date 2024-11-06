994bb97cd099caa798da7a0d450993912711dbe6
Csongrádi Márton <csongradimarci@gmail.com>
2024-11-06 12:29:07 +0100

    ezt otthon csináltam

--- app.py
+++ app.py
@@ -1,44 +1,29 @@
 import random
-
-tablazat = [
-    [1, "kő", "papír"],
-    [0, "kő", "olló"],
-    [-1, "kő", "kő"],
-    [0, "papír", "kő"],
-    [1, "papír", "olló"],
-    [-1, "papír", "papír"],
-    [-1, "olló", "olló"],
-    [1, "olló", "kő"],
-    [0, "olló", "papír"]
-]
+tablazat = [[1, "kő", "papír"], [0, "kő", "olló"], [-1, "kő", "kő"], [0, "papír", "kő"], [1, "papír",
+                                                                                          "olló"], [-1, "papír", "papír"], [-1, "olló", "olló"], [1, "olló", "kő"], [0, "olló", "papír"]]
 
 
 def find(a: str, b: str, i: list):
     for k in i:
-
         if a == k[1] and b == k[2]:
-
             return k[0]
+
     # raise Exception("valami nagyon nem jó")
-
-
 cuccok: list = ["kő", "papír", "olló"]
 user: str = input(f"Addja meg amit választott ({"/".join(cuccok)}): ")
 if user in cuccok:
     randomcucc: str = cuccok[random.randint(1, 3)-1]
-    print("\n\n-------")
-    print(f"választásom: {user}")
-    print(f"Gép választása: {randomcucc}")
+    print("\n\n-------\n",
+          f"választásom: {user}\n", f"Gép választása: {randomcucc}")
     index: int = find(user, randomcucc, tablazat)
     if index == 0:
-        print("Én nyertem")
+        print(" Én nyertem")
     elif index == 1:
-        print("A gép nyert")
+        print(" A gép nyert")
     elif index == -1:
-        print("Döntetlen lett")
+        print(" Döntetlen lett")
     else:
-        print("nagyon nagy baj van!!!")
-    print("-------")
-    print("\n\n")
+        print(" nagyon nagy baj van!!!")
+    print("-------\n\n")
 else:
     print("Valami értelmess opciót kell megadni!!")
